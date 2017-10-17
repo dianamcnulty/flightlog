ef4635c06f95c0f1b238c323c950848707944ce1
dianamcnulty <dianamcnulty@gmail.com>
2017-10-17 19:14:50 -0400

    Create curl script files

---
+++ scripts/flights.sh/create.sh
@@ -0,0 +1,7 @@
+curl --include --request POST "http://localhost:4741/flights/${ID}" \
+--header "Content-Type: application/json" \
+--data '{
+  "flight": {
+
+  }
+}'
---
+++ scripts/flights.sh/update.sh
@@ -0,0 +1,7 @@
+curl --include --request PATCH "http://localhost:4741/flight/${ID}" \
+--header "Content-Type: application/json" \
+--data '{
+  "flight": {
+
+  }
+}'
