4ef4ec396e07a88aab3509cca8626ab15503becb
dianamcnulty <dianamcnulty@gmail.com>
2017-10-20 16:40:45 -0400

    Add curl script for edit

--- scripts/flights.sh/update.sh
+++ scripts/flights.sh/update.sh
@@ -1,7 +1,10 @@
-curl --include --request PATCH "http://localhost:4741/flight/${ID}" \
+curl --include --request PATCH "http://localhost:4741/flights/${ID}" \
 --header "Content-Type: application/json" \
+--header "Authorization: Token token=${TOKEN}" \
 --data '{
   "flight": {
-
+    "flight_type": "'"Imaginary"'",
+    "wing": "'"Pixie Dust"'",
+    "notes": "'"3rd star to the right and straight on to a morning."'"
   }
 }'
